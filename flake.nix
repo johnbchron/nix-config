292fb97a079f9a5da4887c88f6f47b97fad1dd25
John Lewis <github@jlewis.sh>
2023-10-08 17:12:19 -0500

    added home-manager, divided into system and home

--- flake.nix
+++ flake.nix
@@ -2,13 +2,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@attrs: {
     nixosConfigurations.jlewis-laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs // { hardwarePath = ./hardware/jlewis-laptop.nix; };
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./system.nix
+        attrs.home-manager.nixosModules.home-manager ./home.nix
+      ];
     };
   };
 }
