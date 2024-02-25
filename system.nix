102132a797e861c66a2a095dba6f1250d07a2319
John Lewis <github@jlewis.sh>
2024-02-25 11:55:44 -0600

    dump

--- system.nix
+++ system.nix
@@ -11,14 +11,10 @@
     settings = {
       auto-optimise-store = true;
       substituters = [
-        # "https://surrealdb.cachix.org"
-        # "https://devenv.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org/"
       ];
       trusted-public-keys = [
-        # "surrealdb.cachix.org-1:rbm7Qs+s36pxbfk9jhIa5HRld6gZ63koZz1h/9sSxaA="
-        # "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
@@ -91,7 +87,6 @@
     # gedit # text editor
     epiphany # web browser
     geary # email reader
-    gnome-characters
     tali # poker game
     iagno # go game
     hitori # sudoku game
