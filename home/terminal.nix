956978ba536970931fd9a80293863ecddf67aa7a
John Lewis <github@jlewis.sh>
2024-07-24 10:42:59 -0500

    chore: move zsh stuff to dedicated module

--- home/terminal.nix
+++ home/terminal.nix
@@ -3,6 +3,7 @@
     ./terminal/helix.nix
     ./terminal/git.nix
     ./terminal/rss.nix
+    ./terminal/zsh.nix
   ];
 
   home.packages = with pkgs; [
@@ -157,44 +158,6 @@
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    history.size = 3000;
-    shellAliases = rec {
-      e = "exit";
-      cleanup-dev = "rm -rf ~/github/*/{.direnv/,result} ~/playground/*/{.direnv/,result}";
-      clr = "clear";
-      gu = "gitui";
-      l = "eza --all --long --header --total-size --git --git-repos --mounts --hyperlink";
-      ls = "eza";
-      q = "exit";
-      tree = "eza --tree --all --long --header --total-size --git --git-repos --mounts --hyperlink";
-      treeg = tree + " --git-ignore";
-      with-rust = "nix develop \"/home/jlewis/github/with-rust\" --command $SHELL";
-    };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "nix-shell" ];
-      custom = "/home/jlewis/.config/ohmyzsh_custom";
-    };
-    
-    syntaxHighlighting = {
-      enable = true;
-    };
-  };
-
-  # nix-shell custom zsh plugin
-  home.file.ohmyzsh-nix-shell = let
-    nix-shell-repo = pkgs.fetchgit {
-      url = "https://github.com/chisui/zsh-nix-shell";
-      rev = "82ca15e638cc208e6d8368e34a1625ed75e08f90";
-      hash = "sha256-Rtg8kWVLhXRuD2/Ctbtgz9MQCtKZOLpAIdommZhXKdE=";
-    };
-  in {
-    source = "${nix-shell-repo}";
-    target = ".config/ohmyzsh_custom/plugins/nix-shell";
   };
 
   # terminal multiplexer
---
+++ home/terminal/zsh.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }: {
+  programs.zsh = {
+    enable = true;
+    history.size = 3000;
+    shellAliases = rec {
+      e = "exit";
+      cleanup-dev = "rm -rf ~/github/*/{.direnv/,result} ~/playground/*/{.direnv/,result}";
+      clr = "clear";
+      gu = "gitui";
+      l = "eza --all --long --header --total-size --git --git-repos --mounts --hyperlink";
+      ls = "eza";
+      q = "exit";
+      tree = "eza --tree --all --long --header --total-size --git --git-repos --mounts --hyperlink";
+      treeg = tree + " --git-ignore";
+      with-rust = "nix develop \"/home/jlewis/github/with-rust\" --command $SHELL";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "nix-shell" ];
+      custom = "/home/jlewis/.config/ohmyzsh_custom";
+    };
+    
+    syntaxHighlighting = {
+      enable = true;
+    };
+  };
+
+  # nix-shell custom zsh plugin
+  home.file.ohmyzsh-nix-shell = let
+    nix-shell-repo = pkgs.fetchgit {
+      url = "https://github.com/chisui/zsh-nix-shell";
+      rev = "82ca15e638cc208e6d8368e34a1625ed75e08f90";
+      hash = "sha256-Rtg8kWVLhXRuD2/Ctbtgz9MQCtKZOLpAIdommZhXKdE=";
+    };
+  in {
+    source = "${nix-shell-repo}";
+    target = ".config/ohmyzsh_custom/plugins/nix-shell";
+  };
+}
