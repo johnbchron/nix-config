e811871284764443ebd94f42dea4695e2a2976d9
John Lewis <github@jlewis.sh>
2024-10-08 14:36:26 +0200

    feat: more iosevka fiddling

--- extra/iosevka-overlay.nix
+++ extra/iosevka-overlay.nix
@@ -1,6 +1,6 @@
 
 super: prev: let
-  variants = {
+  consolas-based = {
     inherits = "ss03";
     design = {
       g = "single-storey-serifless";
@@ -29,31 +29,62 @@ super: prev: let
       # lig-double-arrow-bar = "without-notch";
     };
   };
+
+  anonymous-pro-based = {
+    inherits = "ss02";
+    design = {
+      capital-c = "serifless";
+      capital-s = "serifless";
+      
+      c = "serifless";
+      f = "flat-hook-serifless-crossbar-at-x-height";
+      r = "top-serifed";
+    
+      asterisk = "turn-hex-high";
+      brace = "curly";
+      question = "smooth";
+      
+      braille-dot = "square";
+
+      lig-equal-chain = "without-notch";
+      lig-hyphen-chain = "without-notch";
+    };
+  };
+
+  menlo-based = {
+    inherits = "ss04";
+
+    design = {
+      braille-dot = "square";
+
+      asterisk = "turn-hex-high";
+      brace = "curly";
+
+      lig-equal-chain = "without-notch";
+      lig-hyphen-chain = "without-notch";
+    };
+  };
+
+  buildPlan = {
+    family = "Iosevka Custom";
+
+    serifs = "sans";
+    noCvSs = true;
+    exportGlyphNames = false;
+
+    variants = menlo-based;
+  };
 in {
   iosevka-term-custom = prev.iosevka.override  {
-    privateBuildPlan = {
+    privateBuildPlan = buildPlan // {
       family = "Iosevka Term Custom";
-
       spacing = "term";
-      serifs = "sans";
-      noCvSs = true;
-      exportGlyphNames = false;
-
-      inherit variants;
+      noLigation = true;
     };
     set = "TermCustom";
   };
   iosevka-custom = prev.iosevka.override  {
-    privateBuildPlan = {
-      family = "Iosevka Custom";
-
-      # spacing = "term";
-      serifs = "sans";
-      noCvSs = true;
-      exportGlyphNames = false;
-
-      inherit variants;
-    };
+    privateBuildPlan = buildPlan;
     set = "Custom";
   };
 }
